c0d76101231ae806ff814d5872559d3d3a45ab74
Joshua Downer <joshua.downer@gmail.com>
2014-06-15 13:35:27 -0400

    traverse: adding text parsing and url filtering

--- katipo/traverse.py
+++ katipo/traverse.py
@@ -1,5 +1,8 @@
 import logging
+import urlparse
 
+from bs4 import BeautifulSoup
+import requests
 import tornado.gen
 
 log = logging.getLogger(__name__)
@@ -14,6 +17,29 @@ class Traverse(object):
 
     @tornado.gen.coroutine
     def run(self):
-        if self.pending:
-            url = self.pending.pop()
-            log.info('searching %s' % (url,))
+        if not self.pending:
+            return
+
+        url = self.pending.pop()
+
+        headers = requests.head(url).headers
+        if not headers.get('content-type', '').startswith('text/html'):
+            return
+
+        log.info('searching %s' % (url,))
+        text = requests.get(url).text
+        soup = BeautifulSoup(text)
+        for a in soup.find_all('a', href=True):
+            link = a['href']
+
+            # ignore links in the same page
+            if link.startswith('#') or link.startswith('javascript'):
+                continue
+
+            # convert relative URLs into absolute URLs
+            if not link.startswith('http'):
+                link = urlparse.urljoin(url, link)
+
+            # add link to pending searches
+            log.info('enqueue %s' % (link,))
+            self._pending.add(link)
