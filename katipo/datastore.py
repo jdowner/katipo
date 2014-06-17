261226ab0c5a942b05b449a685efe261bc6fdfdc
Joshua Downer <joshua.downer@gmail.com>
2014-06-16 21:40:04 -0400

    datastore: first pass implementation

---
+++ katipo/datastore.py
@@ -0,0 +1,46 @@
+import hashlib
+
+import redis
+
+class Datastore(object):
+    def __init__(self, corpus, pool=None):
+        chash = hashlib.md5()
+        for c in corpus:
+            chash.update(c)
+
+        self._corpus_id = chash.hexdigest()
+        self._redis = redis.Redis(connection_pool=pool)
+
+        self._prefix = {term: '%s:%s' % (self._corpus_id, term) for term in
+                ('corpus','searched', 'pending', 'scored')}
+
+        self._redis.sadd(self.prefix['corpus'], corpus)
+
+    @property
+    def prefix(self):
+        return self._prefix
+
+    @property
+    def corpus_id(self):
+        return self._corpus_id
+
+    def corpus(self):
+        return self._redis.smembers(self.prefix['corpus'])
+
+    def add_to_searched(self, url):
+        self._redis.sadd(self.prefix['searched'], url)
+
+    def add_result(self, url, score):
+        self._redis.hmset(self.prefix['scored'], {'url': url, 'score':score})
+
+    def pending(self):
+        return self._redis.smembers(self.prefix['pending'])
+
+    def searched(self):
+        return self._redis.smembers(self.prefix['searched'])
+
+    def pop_pending(self):
+        return self._redis.spop(self.prefix['pending'])
+
+    def push_pending(self, url):
+        self._redis.sadd(self.prefix['pending'], url)
