dfbaec2574b49ac2bf44de8db9700ee30844eda5
Joshua Downer <joshua.downer@gmail.com>
2014-06-18 04:33:08 -0400

    datastore: simplified storage for now

--- katipo/datastore.py
+++ katipo/datastore.py
@@ -1,5 +1,6 @@
 import hashlib
 import logging
+import time
 
 import redis
 
@@ -7,48 +8,22 @@ log = logging.getLogger(__name__)
 
 class Datastore(object):
     def __init__(self, corpus, pool=None):
-        chash = hashlib.md5()
-        for c in corpus:
-            chash.update(c)
-
-        self._corpus_id = chash.hexdigest()
         self._redis = redis.Redis(connection_pool=pool)
-
-        self._prefix = {term: '%s:%s' % (self._corpus_id, term) for term in
-                ('corpus','searched', 'pending', 'score')}
-
-        self._redis.sadd(self.prefix['corpus'], corpus)
-
-    @property
-    def prefix(self):
-        return self._prefix
-
-    @property
-    def corpus_id(self):
-        return self._corpus_id
-
-    def corpus(self):
-        return self._redis.smembers(self.prefix['corpus'])
+        self._redis.sadd('corpus', corpus)
 
     def add_to_searched(self, url):
         log.debug('searched %s' % (url,))
-        self._redis.sadd(self.prefix['searched'], url)
+        self._redis.sadd('searched', url)
 
     def add_result(self, url, score):
-        self._redis.set('%s:%s' % (self.prefix['score'], url), score)
-
-    def pending(self):
-        return self._redis.smembers(self.prefix['pending'])
-
-    def searched(self):
-        return self._redis.smembers(self.prefix['searched'])
+        self._redis.set('score:%s' % (url,), score)
 
     def is_searched(self, url):
-        return self._redis.sismember(self.prefix['searched'], url)
+        return self._redis.sismember('searched', url)
 
     def pop_pending(self):
-        return self._redis.spop(self.prefix['pending'])
+        return self._redis.spop('pending')
 
     def push_pending(self, url):
         log.debug('enqueue %s' % (url,))
-        self._redis.sadd(self.prefix['pending'], url)
+        self._redis.sadd('pending', url)
